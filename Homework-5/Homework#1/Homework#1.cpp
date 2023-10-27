0c6ca36b2a0f8b7c1bf84acb140d46d43acc5382
ZeRg0912 <serg.021@yandex.ru>
2023-10-27 15:08:49 +0300

    start Homework-5

---
+++ Homework-5/Homework#1/Homework#1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class figure {
+protected:
+    int sideCounts = 0;
+    string Name;
+
+public:
+    figure() : figure("Фигура" , 0) {};
+    figure(string FigName, int Sides) {
+        this->Name = FigName;
+        this->sideCounts = Sides;
+    }
+
+    string getName() { 
+        return Name; 
+    }
+
+    int getSidesCount() { 
+        return sideCounts; 
+    }
+
+    void outFig() {
+        cout << this->getName() << ": " << this->getSidesCount() << endl;
+    }
+};
+
+class triangle : public figure {
+public:
+    triangle() : figure("Треугольник", 3) {};
+};
+
+class quadrangle : public figure {
+public:
+    quadrangle() : figure("Четырехугольник", 4) {};
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+    figure fig1;
+    triangle fig2;
+    quadrangle fig3;
+
+    cout << "Количество сторон: \n";
+    fig1.outFig();
+    fig2.outFig();
+    fig3.outFig();
+
+    return 0;
+}
