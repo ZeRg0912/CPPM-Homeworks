e037e08378097121e81a1529474184049eb2bc3b
ZeRg0912 <serg.021@yandex.ru>
2023-11-08 10:41:21 +0300

    complete HW7

---
+++ Homework-7/01/01.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MODE 1
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+#if !defined MODE
+#error "Необходимо определить MODE"
+#elif (MODE == 0)
+    cout << "Работаю в режиме тренировки" << endl;
+#elif (MODE == 1)
+#define add(x, y) ((x) + (y));
+    int x, y;
+    cout << "Работаю в боевом режиме" << endl; 
+    cout << "Введите число 1: ";
+    cin >> x;
+    cout << "Введите число 2: ";
+    cin >> y;
+    cout << "Результат сложения: " << add(x, y);
+    cout << endl;
+#else 
+    cout << "Неизвестный режим. Завершение работы";
+#endif
+    return 0;
+}
---
+++ Homework-7/02/02.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#define SUB(a, b) ((a) - (b))
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+#ifndef SUB 
+#error SUB не определен
+#else
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+#endif
+
+	return 0;
+}
