4c0e59a6f7d9da0aa7d10a4a1bf6c4877ce33632
ZeRg0912 <serg.021@yandex.ru>
2023-11-14 15:11:03 +0300

    complete HW-9

---
+++ Homework-9/01/01.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	double fraction() { return static_cast<double>(numerator_) / static_cast<double>(denominator_); }
+
+	bool operator== (Fraction right) { return fraction() == right.fraction(); }
+
+	bool operator!= (Fraction right) { return !(*this == right); }
+
+	bool operator< (Fraction right) {
+		if (fraction() < right.fraction()) {
+			return true;
+		}
+		else if (fraction() < right.fraction()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool operator> (Fraction right) { return right < *this; }
+	bool operator>= (Fraction right) { return !(*this < right);	}
+	bool operator<= (Fraction right) { return !(right <  * this); }
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	Fraction f1(3, 12);
+	//Fraction f1(6, 11);
+	Fraction f2(5, 8);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
---
+++ Homework-9/02/02.cpp
@@ -0,0 +1,194 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+	int NOD(int numerator, int denominator) {
+		numerator = abs(numerator);
+		denominator = abs(denominator);
+
+		while (numerator > 0 && denominator > 0)
+			if (numerator > denominator) numerator %= denominator;
+			else denominator %= numerator;
+		return numerator + denominator;
+	}
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	Fraction() = default;
+
+	void setFraction(void) {
+		do {
+			cout << "Введите числитель дроби: ";
+			cin >> this->numerator_;
+			if (cin.fail()) {
+				cout << "Введите число!" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			}
+			else {
+				do {
+					cout << "Введите знаменатель дроби: ";
+					cin >> this->denominator_;
+					if (cin.fail()) {
+						cout << "Введите число!" << endl;
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					}
+					else { break; }
+				} while (!isdigit(this->denominator_));
+				break;
+			}
+		} while (!isdigit(this->numerator_));
+		cout << endl;
+	}
+
+	void changeFraction(int newNum, int newDenom) {
+		this->numerator_ = newNum;
+		this->denominator_ = newDenom;
+		cout << "Значение дроби: " << numerator_ << "/" << denominator_ << endl;
+	}
+
+	Fraction getFrac() {
+		return Fraction(numerator_, denominator_);
+	}
+
+	bool operator== (Fraction right) { return NOD(numerator_, denominator_) == right.NOD(numerator_, denominator_); }
+
+	bool operator!= (Fraction right) { return !(*this == right); }
+
+	bool operator< (Fraction right) {
+		if (NOD(numerator_, denominator_) < right.NOD(numerator_, denominator_)) {
+			return true;
+		}
+		else if (NOD(numerator_, denominator_) < NOD(numerator_, denominator_)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool operator> (Fraction right) { return right < *this; }
+	bool operator>= (Fraction right) { return !(*this < right); }
+	bool operator<= (Fraction right) { return !(right < *this); }
+
+	Fraction operator+ (Fraction right) {
+		int newNumerator = (numerator_ * right.denominator_) + (right.numerator_ * denominator_);
+		int newDenominator = (denominator_ * right.denominator_);
+		int NOD_ = NOD(newNumerator, newDenominator);
+		newNumerator = newNumerator / NOD_;
+		newDenominator = newDenominator / NOD_;
+		return Fraction(newNumerator, newDenominator);
+	}
+
+	Fraction operator- (Fraction right) {
+		int newNumerator = (numerator_ * right.denominator_) - (right.numerator_ * denominator_);
+		int newDenominator = (denominator_ * right.denominator_);
+		int NOD_ = NOD(newNumerator, newDenominator);
+		newNumerator = newNumerator / NOD_;
+		newDenominator = newDenominator / NOD_;
+		return Fraction(newNumerator, newDenominator);
+	}
+
+	Fraction operator* (Fraction right) {
+		int newNumerator = (numerator_ * right.numerator_);
+		int newDenominator = (denominator_ * right.denominator_);
+		int NOD_ = NOD(newNumerator, newDenominator);
+		newNumerator = newNumerator / NOD_;
+		newDenominator = newDenominator / NOD_;
+		return Fraction(newNumerator, newDenominator);
+	}
+
+	Fraction operator/ (Fraction right) {
+		int newNumerator = (numerator_ * right.denominator_);
+		int newDenominator = (denominator_ * right.numerator_);
+		int NOD_ = NOD(newNumerator, newDenominator);
+		newNumerator = newNumerator / NOD_;
+		newDenominator = newDenominator / NOD_;
+		return Fraction(newNumerator, newDenominator);
+	}
+
+	Fraction operator! () {
+		int newNumerator = numerator_ * (-1);
+		int newDenominator = denominator_ * (-1);
+		return Fraction(newNumerator, newDenominator);
+	}
+
+	Fraction operator++ () {
+		numerator_ = numerator_ + denominator_;
+		int NOD_ = NOD(numerator_, denominator_);
+		numerator_ = numerator_ / NOD_;
+		denominator_ = denominator_ / NOD_;
+		return Fraction(numerator_, denominator_);
+	}
+
+	Fraction operator++ (int) {
+		numerator_ = numerator_ + denominator_;
+		int NOD_ = NOD(numerator_, denominator_);
+		numerator_ = numerator_ / NOD_;
+		denominator_ = denominator_ / NOD_;
+		return Fraction(numerator_, denominator_);
+	}
+
+	Fraction operator-- () {
+		numerator_ = numerator_ - denominator_;
+		int NOD_ = NOD(numerator_, denominator_);
+		numerator_ = numerator_ / NOD_;
+		denominator_ = denominator_ / NOD_;
+		return Fraction(numerator_, denominator_);
+	}
+
+	Fraction operator-- (int) {
+		numerator_ = numerator_ - denominator_;
+		int NOD_ = NOD(numerator_, denominator_);
+		numerator_ = numerator_ / NOD_;
+		denominator_ = denominator_ / NOD_;
+		return Fraction(numerator_, denominator_);
+	}
+
+	friend ostream& operator<< (ostream& left, Fraction right);
+};
+
+ostream& operator<< (ostream& left, Fraction right) {
+	if (right.denominator_ == 1) {
+		left << right.denominator_;
+	}
+	left << right.numerator_ << "/" << right.denominator_;
+	return left;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	Fraction f1, f2;
+	cout << "Введите первую дробь:\n";
+	f1.setFraction();
+	cout << "Введите вторую дробь:\n";
+	f2.setFraction();
+
+	cout << f1 << " + " << f2 << " = " << f1 + f2 << endl;
+	cout << f1 << " - " << f2 << " = " << f1 - f2 << endl;
+	cout << f1 << " * " << f2 << " = " << f1 * f2 << endl;
+	cout << f1 << " / " << f2 << " = " << f1 / f2 << endl;
+	cout << "++" << f1 << " * " << f2 << " = ";
+	cout << ++f1 * f2 << endl;
+	cout << "Значение дроби 1 = " << f1.getFrac() << endl;
+	cout << f1 << "--" << " * " << f2 << " = ";
+	cout << f1-- * f2 << endl;
+	cout << "Значение дроби 1 = " << f1.getFrac() << endl;
+
+	return 0;
+}
