e014896199b285221b3bc9464f744503741a9830
ZeRg0912 <serg.021@yandex.ru>
2023-11-16 12:28:57 +0300

    test dll library

---
+++ Project/RacingSimulator/RacingLib/RacingLib.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Race/RaceAirGround.h"
+#include "Race/RaceAir.h"
+#include "Race/RaceGround.h"
+
+#define RACINGLIBRARY_EXPORTS
+
+#ifdef RACINGLIBRARY_EXPORTS
+#define RACINGLIBRARY_API __declspec(dllexport)
+#else 
+#define RACINGLIBRARY_API __declspec(dllimport)
+#endif
+
+RACINGLIBRARY_API int setDistance();
+
+class RACINGLIBRARY_API  Race {
+public:
+	void begin();
+	void printMenuTable();
+	void doRestart(Race& race);
+};
