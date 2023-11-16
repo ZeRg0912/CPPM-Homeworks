e56e221d95f48293ef58e77e44254631810a42e8
ZeRg0912 <serg.021@yandex.ru>
2023-11-16 14:55:37 +0300

    try to cmake add libs

---
+++ Project/Test/RacingSimulatorCMake/RacingSimulator.cpp
@@ -0,0 +1,16 @@
+﻿//#pragma comment(lib,"RacingLib.lib")
+#include "RacingLib.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+
+    Race race;
+    race.begin();
+
+    return 0;
+}
